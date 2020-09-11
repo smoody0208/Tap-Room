1654142c52899b1a7583efd6ca8e516466ca196c
Spencer Moody <spencer.moody@outlook.com>
2020-09-11 12:20:04 -0700

    Add code to TapList component

---
+++ src/components/TapList.js
@@ -0,0 +1,29 @@
+import React from "react";
+import Tap from "./Tap";
+import PropTypes from "prop-types";
+
+function TapList(props){
+  return (
+    <React.Fragment>
+      <hr/>
+      {props.tapList.map((tap) =>
+        <Tap
+        whenTapClicked = {props.onTapSelection}
+        brand={tap.brand}
+        name={tap.name}
+        price={tap.price}
+        alcoholContent={tap.alcoholContent}
+        pintsLeft={tap.pintsLeft}
+        id={tap.id}
+        key={tap.id}/>
+      )}
+    </React.Fragment>
+  );
+}
+
+TapList.propTypes = {
+  tapList: PropTypes.array,
+  onTapSelection: PropTypes.func
+};
+
+export default TapList;
