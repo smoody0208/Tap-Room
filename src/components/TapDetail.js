16aae35e68bbe2edef625f8c648f6f56632f6b2a
Spencer Moody <spencer.moody@outlook.com>
2020-09-11 12:37:12 -0700

    Add code to TapDetail component

---
+++ src/components/TapDetail.js
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+function TapDetail(props) {
+  const { tap, onClickingDelete } = props;
+  return (
+    <React.Fragment>
+      <h1>Tap Info</h1>
+      <p>Name: {tap.name}</p>
+      <p>Brand: {tap.brand}</p>
+      <p>Price: {tap.price}</p>
+      <p>Alcohol Content: {tap.alcoholContent}</p>
+      <button onClick={props.onClickingEdit}>Update Tap</button>
+      <button onClick={() => onClickingDelete(tap.id)}>Delete Tap</button>
+      <hr />
+    </React.Fragment>
+  );
+}
+
+TapDetail.propTypes = {
+  tap: PropTypes.object,
+  onClickingDelete: PropTypes.func,
+  onClickingEdit: PropTypes.func
+};
+
+export default TapDetail;
