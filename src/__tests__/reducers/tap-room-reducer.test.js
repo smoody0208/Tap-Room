8d97fe5d505e575220e175582d6a4825cfa8af31
Spencer Moody <spencer.moody@outlook.com>
2020-10-02 14:54:12 -0700

    New test successfully failed

--- src/__tests__/reducers/tap-room-reducer.test.js
+++ src/__tests__/reducers/tap-room-reducer.test.js
@@ -1,11 +1,9 @@
 import tapRoomReducer from '../../reducers/tap-room-reducer';
 
-import ticketListReducer from '../../reducers/ticket-list-reducer';
-
 describe('tapRoomReducer', () => {
 
   let action;
-  const ticketData = {
+  const tapData = {
     name: 'Rich and Rare Canadian Whiskey',
     brand: 'Whiskey',
     price: '$20',
@@ -15,5 +13,29 @@ describe('tapRoomReducer', () => {
 
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(tapRoomReducer({}, { type: null })).toEqual({});
-  });
-});
+  })
+
+
+    test('Should successfully add new ticket data to masterTapList', () => {
+      const { name, brand, price, alcoholContent, id } = tapData;
+      action = {
+        type: 'ADD_TAP',
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        id: id
+      };
+  
+      expect(tapRoomReducer({}, action)).toEqual({
+        [id] : {
+          name: name,
+          brand: brand,
+          price: price,
+          alcoholContent: alcoholContent,
+          id: id
+        }
+      });
+    });
+  
+  });  
