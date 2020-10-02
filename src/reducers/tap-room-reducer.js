980bb3dee06b06c9e3e438c71367d972f0553417
Spencer Moody <spencer.moody@outlook.com>
2020-10-02 14:56:46 -0700

    Update tap room reducer so test is now passing

--- src/reducers/tap-room-reducer.js
+++ src/reducers/tap-room-reducer.js
@@ -1,3 +1,17 @@
 export default (state = {}, action) => {
-  return state;
+  const { name, brand, price, alcoholContent, id } = action;
+  switch (action.type) {
+  case 'ADD_TAP':
+    return Object.assign({}, state, {
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        id: id
+      }
+    });
+  default:
+    return state;
+  }
 };
