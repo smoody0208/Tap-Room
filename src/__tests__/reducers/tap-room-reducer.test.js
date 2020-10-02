913d51ad918709364fef6d4dd466e4c0cc93c6fe
Spencer Moody <spencer.moody@outlook.com>
2020-10-02 13:10:53 -0700

    Add test for adding taps

--- src/__tests__/reducers/tap-room-reducer.test.js
+++ src/__tests__/reducers/tap-room-reducer.test.js
@@ -1,6 +1,18 @@
 import tapRoomReducer from '../../reducers/tap-room-reducer';
 
+import ticketListReducer from '../../reducers/ticket-list-reducer';
+
 describe('tapRoomReducer', () => {
+
+  let action;
+  const ticketData = {
+    name: 'Rich and Rare Canadian Whiskey',
+    brand: 'Whiskey',
+    price: '$20',
+    alcoholContent: '80 proof',
+    id: 1
+  };
+
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(tapRoomReducer({}, { type: null })).toEqual({});
   });
