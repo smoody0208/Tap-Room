83d277dcbb66184f08a21adf26e29e70bd6c3ed8
Spencer Moody <spencer.moody@outlook.com>
2020-10-04 16:57:01 -0700

    Create TOGGLE_FORM action creator and test/pass test

--- src/__tests__/actions/index.test.js
+++ src/__tests__/actions/index.test.js
@@ -7,4 +7,9 @@ describe('tap actions', () => {
       id: 1
     });
   });
+  it('toggleForm should create TOGGLE_FORM action', () => {
+    expect(actions.toggleForm()).toEqual({
+      type: 'TOGGLE_FORM'
+    });
+  });
 });
--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,7 @@ export const deleteTap = id => ({
   type: 'DELETE_TAP',
   id
 });
+
+export const toggleForm = () => ({
+  type: 'TOGGLE_FORM'
+});
